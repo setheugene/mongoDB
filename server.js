c5aaac261949da2e8735274c14c15e5f2f3cef27
setheugene <seth.eugene@icloud.com>
2019-04-06 15:32:39 -0500

    processenv

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/newsscraper";
 
 mongoose.connect(MONGODB_URI);
 
